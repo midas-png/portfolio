cd9fd18e39e919d973a9382ace2f91673c07ad2c
Yan <y4nlvn@gmail.com>
2023-03-16 14:43:35 +0300

    lazy show added for swiper

--- src/components/slider/index.tsx
+++ src/components/slider/index.tsx
@@ -9,38 +9,41 @@ import {
   NotificationTopBorderWrapper,
   NotificationTopBorder,
 } from './styles';
+import { LazyShow } from 'components';
 import { PROJECTS } from 'data';
 import { Title } from 'ui';
 import { Project } from 'components';
 
 export const Slider: FC = () => {
   return (
-    <ComponentWrapper>
-      <Title fontSize={60}>What I created</Title>
-      <ComponentSliderWrapper>
-        <SliderWrapper loop={true} spaceBetween={50}>
-          {PROJECTS.map(
-            ({ name, company, description, link, image }, index) => (
-              <Slide key={Math.random()}>
-                <Project
-                  name={name}
-                  company={company}
-                  description={description}
-                  link={link}
-                  image={image}
-                  reverse={index % 2 === 0}
-                />
-              </Slide>
-            )
-          )}
-        </SliderWrapper>
-        <NotificationWrapper>
-          <NotificationTopBorderWrapper>
-            <NotificationTopBorder />
-          </NotificationTopBorderWrapper>
-          <NotificationSwiper>SWIPE</NotificationSwiper>
-        </NotificationWrapper>
-      </ComponentSliderWrapper>
-    </ComponentWrapper>
+    <LazyShow direction='left'>
+      <ComponentWrapper>
+        <Title fontSize={60}>What I created</Title>
+        <ComponentSliderWrapper>
+          <SliderWrapper loop={true} spaceBetween={50}>
+            {PROJECTS.map(
+              ({ name, company, description, link, image }, index) => (
+                <Slide key={Math.random()}>
+                  <Project
+                    name={name}
+                    company={company}
+                    description={description}
+                    link={link}
+                    image={image}
+                    reverse={index % 2 === 0}
+                  />
+                </Slide>
+              )
+            )}
+          </SliderWrapper>
+          <NotificationWrapper>
+            <NotificationTopBorderWrapper>
+              <NotificationTopBorder />
+            </NotificationTopBorderWrapper>
+            <NotificationSwiper>SWIPE</NotificationSwiper>
+          </NotificationWrapper>
+        </ComponentSliderWrapper>
+      </ComponentWrapper>
+    </LazyShow>
   );
 };
